ae85201b7980613ee9c457f1f9d823dcb4400d17
Nenad <ristovn18@gmail.com>
2020-07-29 15:05:54 +0200

    initial

---
+++ Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class Employee
+    {
+        
+            
+
+        private String name;
+        private int salary;
+        private string date_joined;
+        private String[] skills= new String[10];
+
+        public Employee(String name, int salary, string date_joined, String[] skills)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.date_joined = date_joined;
+            this.skills = skills;
+          
+            ;
+        }
+        //printing the name
+        public void Get_name()
+        {
+            Console.WriteLine(name);
+        }
+        //printing the salary
+        public void Get_salary()
+        {
+            Console.WriteLine(salary);
+        }
+        //printing the date he/she joined
+        public void Get_date_joined()
+        {
+            Console.WriteLine(date_joined);
+        }
+        //method fot raising the salary
+        public void Pay_raise(int raise)
+        {
+            salary += raise;
+        }
+        //printing the  skills
+        public void Get_skills()
+        {
+            Console.WriteLine("This are the skills:");
+            foreach (String i in skills)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        //printing all the information with ToString()
+        public override string ToString()
+        {
+            return $"Name: {name}\n Salary: {salary}\n Date joined {date_joined}";
+        }
+        //method for adding skills
+        public void Add_skills(int x, String skill)
+        {
+            skills[x] = skill;
+        }
+
+        // method for removing skills
+
+        // methon for altering skills
+        public void Alter_skills(int x, String skill)
+        {
+            skills[x] = skill;
+        }
+        //add name
+        public void Add_name(String new_name)
+        {
+            name = new_name;
+        }
+
+        //add salary
+        public void Add_salary(int new_salary)
+        {
+            salary = new_salary;
+        }
+        
+        //add date joined
+        public void Add_date_jooined(String new_date)
+        {
+            date_joined = new_date;
+        }
+
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String name, date;
+            String[] skills = new String[10];
+            
+            int salary;
+            
+            
+            //gathering informations for the employee and creating an object
+            Console.WriteLine("Input name for the employee");
+            name = Console.ReadLine();
+            Console.WriteLine("Input salary for the employee");
+            salary =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the date the employee has joined the company");
+            date = Console.ReadLine();
+            
+            //adding skills to the employees object
+            Console.WriteLine("How many skills does the employee has (MAX 10");
+            int num_skills = Convert.ToInt32(Console.ReadLine());
+            for (int y=0; y<=num_skills; y++)
+            {
+                Console.WriteLine("Input the skill");
+                String skill = Console.ReadLine();
+                skills[y]=skill;
+            }
+            Employee obj1 = new Employee(name, salary, date, skills);
+            Console.WriteLine("Input username for the employee");
+            String username = Console.ReadLine();
+            Console.WriteLine("Input password for the employee");
+            String password = Console.ReadLine();
+            Console.WriteLine("Input email for the employee");
+            String email = Console.ReadLine();
+
+            //Profile.Create_user_profile();//PROBLEM
+            Console.WriteLine("_________________________________________");
+            obj1.Get_name();
+            obj1.Get_salary();
+            obj1.Get_skills();
+            Console.WriteLine("_________________________________________");
+            obj1.ToString();
+            Console.WriteLine("How much would the pay raise be?");
+            int raise = Convert.ToInt32(Console.ReadLine());
+            obj1.Pay_raise(raise);
+            obj1.ToString();
+        }}
+}
+
